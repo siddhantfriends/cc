6935389b020209dc6e96bb43c3d564181446d706
Siddhant Baviskar <siddhantbaviskar@outlook.com>
2019-11-13 12:31:38 +0530

    Comments

---
+++ 2019-11-13 12:26:08 comments/MultiLineComments.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+/* The function below will print Hello World! */
+int main() {
+    /* The code below will print the words Hello World!
+    to the screen, and it's amazing */
+    cout << "Hello World!";
+    return 0;
+}
---
+++ 2019-11-13 12:26:08 comments/SingleLineComments.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // This is a comment
+    cout << "Hello World!"; // This is a comment
+    return 0;
+}
